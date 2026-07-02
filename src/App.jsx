c9971be74b3ff90dea749863e23ce90041979312
ROHAN-8504 <203j1a4207@raghuinstech.com>
2026-07-02 11:58:10 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <Header/>
     <Home/>
      <h1>cards</h1>
-     <Card/>
-     <Card/>
+     <Card img="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/6/15/26ecae56-6196-4204-a1c0-d7d984208ecf_217542.JPG"  title="Pizza Hut" address='vijaywada'  />
+     <Card img="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/pocpd4knzgptsuguulhv"  title="bbq"  address="srm ap"  />
      
     </>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,15 @@
 
-function Card() {
+function Card(props) {
   return (
     <>
     <span>
-    <img src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_600,h_400/v1700575709/a489b494e5017dbe74aea23b7fd5ff91.jpg" alt="" />
-     <p>Biryani Box</p>
-     <p>New Bapuji Nagar, Bhubaneswar</p>
+    <img src={props.img} alt="" />
+     <p>{props.title}</p>
+     <p>{props.address}</p>
     </span>
     </>
   )
 }
 
-export default Card 
+export default Card
+
